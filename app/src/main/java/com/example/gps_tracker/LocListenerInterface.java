ceb02d8863c2333ebb4c14089a5b2d0e571fa25c
Alexander <Sandr1109@yandex.ru>
2023-05-17 23:06:42 +0400

    First_Commit

---
+++ app/src/main/java/com/example/gps_tracker/LocListenerInterface.java
@@ -0,0 +1,7 @@
+package com.example.gps_tracker;
+
+import android.location.Location;
+
+public interface LocListenerInterface {
+    public void onLocationChanged(Location loc);
+}
---
+++ app/src/main/java/com/example/gps_tracker/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.gps_tracker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocListenerInterface {
+    private LocationManager locationManager;
+    private TextView tvDistance, tvVelocity;
+    private Location lastLocation;
+    private MyLogListener myLocListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        myLocListener = new MyLogListener();
+        myLocListener.setLocListenerInterface(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 100 && grantResults[0] == RESULT_OK){
+
+        } else {
+            Toast.makeText(this, "NO GPS PERMISSION", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void checkPermission(){                     //menyalas
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 100);
+        } else {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 125, 1, myLocListener);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+
+    }
+}
+
